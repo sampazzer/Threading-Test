8b873f69af56de58c86926de43dc4cb74f7535d4
sampazzer <sampazzer@googlemail.com>
2023-11-19 06:01:04 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import time
 import threading
 import random
+
 start_vent_thread_event = threading.Event()
 open_vent_event = threading.Event()
 close_vent_event = threading.Event()
@@ -9,33 +10,36 @@ close_vent_event = threading.Event()
 ##an event to start and stop by using set/clear/wait. set turns wait to true
 ##clear turns wait to false. This is handy in a while loop.
 
+
 def temperature_readout():
-    my_thread = threading.Thread(target=open_close_vent_thread,daemon=True)
+    my_thread = threading.Thread(target=open_close_vent_thread)
     my_thread.start()
     while True:
         command = int(input("1 for open, 2 for close: "))
-        if command is 1:
+        if command == 1:
             start_vent_thread_event.set()
             open_vent_event.set()
-        elif command is 2:
+        elif command == 2:
             start_vent_thread_event.set()
             close_vent_event.set()
         time.sleep(4)
 
 
 def open_close_vent_thread():
-    while start_vent_thread_event.wait():
+    while True:
+        start_vent_thread_event.wait()
         print("open close thread started")
-        if open_vent_event.wait():
-            time.sleep(2)
+        if open_vent_event.is_set():
             print("I am opening the vent")
+            time.sleep(2)
             open_vent_event.clear()
 
-        if close_vent_event.wait():
-            time.sleep(2)
+        if close_vent_event.is_set():
             print("I am closing the vent")
+            time.sleep(2)
             close_vent_event.clear()
         start_vent_thread_event.clear()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     temperature_readout()
